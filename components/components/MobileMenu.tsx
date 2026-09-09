f38b2d137a223310429d48f9c3a3e34e40d431c2
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-09 23:49:59 +0330

    Create MobileMenu.tsx

---
+++ components/components/MobileMenu.tsx
@@ -0,0 +1,25 @@
+export default function MobileMenu() {
+  return (
+    <nav>
+      <a>
+        🏠
+        <span>خانه</span>
+      </a>
+
+      <a>
+        📊
+        <span>سیگنال‌ها</span>
+      </a>
+
+      <a>
+        🤖
+        <span>ربات‌ها</span>
+      </a>
+
+      <a>
+        👤
+        <span>پروفایل</span>
+      </a>
+    </nav>
+  );
+}
