3c52fff48520ca3b59ee2e7c52213561dc87826f
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-09 23:54:24 +0330

    Update MobileMenu.tsx

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,25 +1,57 @@
-export default function MobileMenu() {
+import Navbar from "@/components/Navbar";
+import MobileMenu from "@/components/MobileMenu";
+
+export default function Home() {
   return (
-    <nav>
-      <a>
-        🏠
-        <span>خانه</span>
-      </a>
+    <main>
 
-      <a>
-        📊
-        <span>سیگنال‌ها</span>
-      </a>
+      <Navbar />
 
-      <a>
-        🤖
-        <span>ربات‌ها</span>
-      </a>
+      <section>
+        <h1>
+          Trading AI Platform
+        </h1>
 
-      <a>
-        👤
-        <span>پروفایل</span>
-      </a>
-    </nav>
+        <p>
+          پلتفرم هوشمند تحلیل بازار، سیگنال و مدیریت معاملات
+        </p>
+      </section>
+
+      <section>
+        <h2>
+          امکانات ما
+        </h2>
+
+        <div>
+          <h3>
+            🤖 ربات های هوش مصنوعی
+          </h3>
+          <p>
+            تحلیل بازار و کمک به تصمیم‌گیری معاملاتی
+          </p>
+        </div>
+
+        <div>
+          <h3>
+            📊 سیگنال های معاملاتی
+          </h3>
+          <p>
+            فارکس، کریپتو و طلا
+          </p>
+        </div>
+
+        <div>
+          <h3>
+            ⚡ ربات معامله‌گر
+          </h3>
+          <p>
+            مدیریت معاملات خودکار در آینده
+          </p>
+        </div>
+      </section>
+
+      <MobileMenu />
+
+    </main>
   );
 }
