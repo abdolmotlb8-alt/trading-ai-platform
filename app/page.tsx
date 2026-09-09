1843cf33e6109e7e56c9d247032a7fed4b8b5041
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-09 22:34:31 +0330

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Trading AI Platform</h1>
+
+      <p>
+        AI Trading Signals, Bots and Market Intelligence
+      </p>
+    </main>
+  );
+}
