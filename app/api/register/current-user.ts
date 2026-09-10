cd0d1250e844d44dc538405bc8056aa597a92660
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-10 18:35:03 +0330

    Update current-user.ts

--- app/api/register/current-user.ts
+++ app/api/register/current-user.ts
@@ -1,1 +1,25 @@
+import { prisma } from "@/lib/prisma";
+import { getSession } from "@/lib/session";
 
+export async function getCurrentUser() {
+  const userId = await getSession();
+
+  if (!userId) {
+    return null;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      plan: true
+    }
+  });
+
+  return user;
+}
