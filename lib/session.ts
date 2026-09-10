4bd0fa5d57aa61b0def60647a9e93e755abac44a
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-11 01:30:37 +0330

    Update session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -1,14 +1,71 @@
+import { cookies } from "next/headers";
+
+
+const SESSION_NAME = "trading_session";
+
+
 export async function createSession(userId: string) {
-  return {
+
+  const cookieStore = await cookies();
+
+
+  cookieStore.set(
+    SESSION_NAME,
     userId,
-    createdAt: new Date(),
+    {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 7,
+      path: "/",
+    }
+  );
+
+
+  return {
+    userId
   };
+
 }
+
+
 
 export async function getSession() {
-  return null;
+
+  const cookieStore = await cookies();
+
+
+  const session =
+    cookieStore.get(SESSION_NAME);
+
+
+  if (!session) {
+
+    return null;
+
+  }
+
+
+  return {
+
+    userId: session.value
+
+  };
+
 }
 
+
+
 export async function deleteSession() {
+
+  const cookieStore = await cookies();
+
+
+  cookieStore.delete(
+    SESSION_NAME
+  );
+
+
   return true;
+
 }
