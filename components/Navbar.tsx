2796a6ab0d94176cc39cc9ce25d53cfbc5c61f78
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-10 14:08:05 +0330

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,42 @@
 export default function Navbar() {
   return (
-    <nav>
-      <h2>
-        Trading AI Platform
-      </h2>
+    <header>
+      <nav>
 
-      <div>
-        <a>خانه</a>
-        <a>سیگنال‌ها</a>
-        <a>ربات‌ها</a>
-        <a>ورود</a>
-      </div>
-    </nav>
-  )
+        <h2>
+          🤖 Trading AI
+        </h2>
+
+
+        <div>
+
+          <a href="/">
+            خانه
+          </a>
+
+
+          <a href="/dashboard">
+            داشبورد
+          </a>
+
+
+          <a href="/signals">
+            سیگنال‌ها
+          </a>
+
+
+          <a href="/bots">
+            ربات‌ها
+          </a>
+
+
+          <a href="/profile">
+            پروفایل
+          </a>
+
+        </div>
+
+      </nav>
+    </header>
+  );
 }
