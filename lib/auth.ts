8bb7a9f10cbe6d1911ced587f24819d6ed9fbd10
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-10 17:47:50 +0330

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,35 @@
+export type UserRole =
+  | "USER"
+  | "VIP"
+  | "ADMIN";
 
+
+export function hasAccess(
+  role: UserRole,
+  requiredRole: UserRole
+) {
+
+  if (role === "ADMIN") {
+    return true;
+  }
+
+
+  if (
+    role === "VIP" &&
+    requiredRole === "VIP"
+  ) {
+    return true;
+  }
+
+
+  if (
+    role === "USER" &&
+    requiredRole === "USER"
+  ) {
+    return true;
+  }
+
+
+  return false;
+
+}
