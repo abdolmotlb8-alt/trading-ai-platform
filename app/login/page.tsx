4cecf9908104c6ca49b85c606a64e6654eef8419
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-10 16:11:31 +0330

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,57 +1,10 @@
+import AuthForm from "@/components/AuthForm";
+
 export default function LoginPage() {
   return (
     <main>
 
-      <h1>
-        ورود به حساب کاربری
-      </h1>
-
-      <p>
-        وارد حساب خود شوید و به خدمات Trading AI دسترسی پیدا کنید.
-      </p>
-
-
-      <section>
-
-        <label>
-          ایمیل
-        </label>
-
-        <input
-          type="email"
-          placeholder="ایمیل خود را وارد کنید"
-        />
-
-
-        <label>
-          رمز عبور
-        </label>
-
-        <input
-          type="password"
-          placeholder="رمز عبور خود را وارد کنید"
-        />
-
-
-        <button>
-          ورود
-        </button>
-
-      </section>
-
-
-      <section>
-
-        <p>
-          حساب ندارید؟
-        </p>
-
-        <button>
-          ثبت نام
-        </button>
-
-      </section>
-
+      <AuthForm />
 
     </main>
   );
