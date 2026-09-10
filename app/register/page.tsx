b065a20ecde944b8c0d5c3c7f70ce336d47cd750
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-10 16:15:10 +0330

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,63 +1,10 @@
+import AuthForm from "@/components/AuthForm";
+
 export default function RegisterPage() {
   return (
     <main>
 
-      <h1>
-        ثبت نام
-      </h1>
-
-      <p>
-        ساخت حساب کاربری برای استفاده از خدمات Trading AI
-      </p>
-
-
-      <section>
-
-        <label>
-          نام کاربر
-        </label>
-
-        <input
-          type="text"
-          placeholder="نام خود را وارد کنید"
-        />
-
-
-        <label>
-          ایمیل
-        </label>
-
-        <input
-          type="email"
-          placeholder="ایمیل خود را وارد کنید"
-        />
-
-
-        <label>
-          رمز عبور
-        </label>
-
-        <input
-          type="password"
-          placeholder="رمز عبور"
-        />
-
-
-        <button>
-          ایجاد حساب
-        </button>
-
-      </section>
-
-
-      <section>
-
-        <p>
-          با ثبت نام، به داشبورد، سیگنال‌ها و خدمات ویژه دسترسی خواهید داشت.
-        </p>
-
-      </section>
-
+      <AuthForm />
 
     </main>
   );
