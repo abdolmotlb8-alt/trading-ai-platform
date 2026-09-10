a9cdf62cdd7480bfbf3fcb3108a5a92019fa450a
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-10 19:14:21 +0330

    Update page.tsx

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,1 +1,70 @@
+import { prisma } from "@/lib/prisma";
 
+export default async function AdminUsersPage() {
+
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      plan: true,
+    },
+  });
+
+
+  return (
+
+    <main>
+
+      <h1>
+        مدیریت کاربران
+      </h1>
+
+
+      <section>
+
+        {users.length === 0 ? (
+
+          <p>
+            هنوز کاربری ثبت نشده است.
+          </p>
+
+        ) : (
+
+          users.map((user) => (
+
+            <div key={user.id}>
+
+              <p>
+                نام: {user.name}
+              </p>
+
+              <p>
+                ایمیل: {user.email}
+              </p>
+
+              <p>
+                نقش: {user.role}
+              </p>
+
+              <p>
+                پلن: {user.plan}
+              </p>
+
+              <hr />
+
+            </div>
+
+          ))
+
+        )}
+
+      </section>
+
+
+    </main>
+
+  );
+
+}
