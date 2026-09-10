dd5a849aa4e9dda934e35bc790dcaa3bd5e019d1
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-11 02:37:36 +0330

    Update route.ts

--- app/api/register/api/login/route.ts
+++ app/api/register/api/login/route.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 import { createSession } from "@/lib/session";
 
+
 export async function POST(request: Request) {
 
   try {
@@ -11,86 +12,120 @@ export async function POST(request: Request) {
 
     const {
       email,
-      password,
+      password
     } = body;
 
 
     if (!email || !password) {
+
       return NextResponse.json(
         {
-          message: "ایمیل و رمز عبور الزامی است",
+          message: "ایمیل و رمز عبور الزامی است"
         },
         {
-          status: 400,
+          status: 400
         }
       );
+
     }
 
 
-    const user = await prisma.user.findUnique({
-      where:{
-        email: email.trim().toLowerCase()
-      }
-    });
+
+    const normalizedEmail =
+      email.trim().toLowerCase();
 
 
-    if(!user){
+
+    const user =
+      await prisma.user.findUnique({
+
+        where: {
+          email: normalizedEmail
+        }
+
+      });
+
+
+
+    if (!user) {
+
       return NextResponse.json(
         {
-          message:"ایمیل یا رمز عبور اشتباه است"
+          message: "ایمیل یا رمز عبور اشتباه است"
         },
         {
-          status:401
+          status: 401
         }
       );
+
     }
 
 
-    const checkPassword = await bcrypt.compare(
-      password,
-      user.password
-    );
+
+    const validPassword =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
 
 
-    if(!checkPassword){
+
+    if (!validPassword) {
+
       return NextResponse.json(
         {
-          message:"ایمیل یا رمز عبور اشتباه است"
+          message: "ایمیل یا رمز عبور اشتباه است"
         },
         {
-          status:401
+          status: 401
         }
       );
+
     }
+
 
 
     await createSession(user.id);
 
 
-    return NextResponse.json({
-      message:"ورود موفق بود",
-      user:{
-        id:user.id,
-        name:user.name,
-        email:user.email,
-        role:user.role,
-        plan:user.plan
-      }
-    });
-
-
-  } catch(error){
-
-    console.log(error);
 
     return NextResponse.json(
       {
-        message:"خطای سرور"
+        message: "ورود موفق بود",
+
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+          plan: user.plan
+        }
       },
       {
-        status:500
+        status: 200
       }
     );
+
+
+
+  } catch (error) {
+
+
+    console.log(
+      "LOGIN ERROR:",
+      error
+    );
+
+
+    return NextResponse.json(
+      {
+        message: "خطای سرور"
+      },
+      {
+        status: 500
+      }
+    );
+
   }
 
 }
