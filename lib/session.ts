a3784436d51d5fe2804089a11efb9bf0cf20385a
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-11 02:35:53 +0330

    Update session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -1,8 +1,10 @@
 import { cookies } from "next/headers";
+import crypto from "crypto";
+
 
 export async function createSession(userId: string) {
 
-  const token = userId;
+  const token = crypto.randomUUID();
 
   const cookieStore = await cookies();
 
@@ -18,25 +20,32 @@ export async function createSession(userId: string) {
     }
   );
 
-  return token;
+
+  return {
+    token,
+    userId,
+  };
 }
+
 
 
 export async function getSession() {
 
   const cookieStore = await cookies();
 
-  const session =
-    cookieStore.get("session");
+  const session = cookieStore.get("session");
+
 
   if (!session) {
     return null;
   }
 
+
   return {
-    userId: session.value,
+    token: session.value,
   };
 }
+
 
 
 export async function deleteSession() {
