e0955547063e5e3c34530d462d22b7ad80ee4a1e
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-11 01:50:35 +0330

    Update session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -1,47 +1,49 @@
 import { cookies } from "next/headers";
-import crypto from "crypto";
-
-const SESSION_COOKIE = "session";
 
 export async function createSession(userId: string) {
-  const token = crypto.randomBytes(32).toString("hex");
+
+  const token = userId;
 
   const cookieStore = await cookies();
 
-  cookieStore.set(SESSION_COOKIE, token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge: 60 * 60 * 24 * 7,
-    path: "/",
-  });
-
-  return {
+  cookieStore.set(
+    "session",
     token,
-    userId,
-  };
+    {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 7,
+      path: "/",
+    }
+  );
+
+  return token;
 }
 
 
 export async function getSession() {
+
   const cookieStore = await cookies();
 
-  const token = cookieStore.get(SESSION_COOKIE);
+  const session =
+    cookieStore.get("session");
 
-  if (!token) {
+  if (!session) {
     return null;
   }
 
   return {
-    token: token.value,
+    userId: session.value,
   };
 }
 
 
 export async function deleteSession() {
+
   const cookieStore = await cookies();
 
-  cookieStore.delete(SESSION_COOKIE);
+  cookieStore.delete("session");
 
   return true;
 }
