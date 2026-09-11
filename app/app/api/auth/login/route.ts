ad47b2dfd61530b87cd3216010e1c1a5445ddf45
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-11 21:47:13 +0330

    Update route.ts

--- app/app/api/auth/login/route.ts
+++ app/app/api/auth/login/route.ts
@@ -1,1 +1,131 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+import { createSession } from "@/lib/session";
 
+
+export async function POST(request: Request) {
+
+  try {
+
+    const body = await request.json();
+
+    const {
+      email,
+      password
+    } = body;
+
+
+    if (!email || !password) {
+
+      return NextResponse.json(
+        {
+          message: "ایمیل و رمز عبور الزامی است"
+        },
+        {
+          status: 400
+        }
+      );
+
+    }
+
+
+    const normalizedEmail =
+      email.trim().toLowerCase();
+
+
+
+    const user =
+      await prisma.user.findUnique({
+
+        where: {
+          email: normalizedEmail
+        }
+
+      });
+
+
+
+    if (!user) {
+
+      return NextResponse.json(
+        {
+          message: "ایمیل یا رمز عبور اشتباه است"
+        },
+        {
+          status: 401
+        }
+      );
+
+    }
+
+
+
+    const passwordMatch =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
+
+
+
+    if (!passwordMatch) {
+
+      return NextResponse.json(
+        {
+          message: "ایمیل یا رمز عبور اشتباه است"
+        },
+        {
+          status: 401
+        }
+      );
+
+    }
+
+
+
+    await createSession(user.id);
+
+
+
+    return NextResponse.json(
+      {
+        message: "ورود موفق بود",
+
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+          plan: user.plan
+        }
+
+      },
+      {
+        status: 200
+      }
+    );
+
+
+
+  } catch (error) {
+
+
+    console.error(
+      "LOGIN ERROR:",
+      error
+    );
+
+
+    return NextResponse.json(
+      {
+        message: "خطای داخلی سرور"
+      },
+      {
+        status: 500
+      }
+    );
+
+  }
+
+}
