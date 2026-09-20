b70978d9330df6b4822a221ad375bb6907a347f3
abdolmotlb8-alt <abdolmotlb8@gmail.com>
2026-09-21 00:51:33 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  poweredByHeader: false,
+
+  compress: true,
+
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+
+  images: {
+    unoptimized: false,
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 module.exports = nextConfig;
